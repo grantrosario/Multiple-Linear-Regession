564eac896051a3bec80acacc31e76aac939c83c1
Grant Rosario <grantster1124@aol.com>
2017-02-02 17:31:18 -0500

    Added Backward Elimination to find optimal model

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -11,7 +11,7 @@ dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 Y = dataset.iloc[:, 4].values
 
-## Encoding categorical data
+# Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
@@ -41,3 +41,27 @@ regressor.fit(X_train, Y_train)
 
 # Predicting the test set results
 y_pred = regressor.predict(X_test)
+
+# Building the optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+# Remove highest p-value > 0.05
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+# Remove highest p-value > 0.05
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+# Remove highest p-value > 0.05
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+# No p-value > 0.05; model complete
+X_opt = X[:, [0, 3]]
+regressor_OLS = sm.OLS(endog = Y, exog = X_opt).fit()
+regressor_OLS.summary()
+
