e1a48a53794685259ee465c420fc6d29987bd59b
Grant Rosario <grantster1124@aol.com>
2017-02-06 23:56:17 -0500

    Preprocessed data

---
+++ multiple_linear_regression.R
@@ -0,0 +1,22 @@
+# Multiple Linear Regression
+
+# Importing the dataset
+dataset = read.csv('50_Startups.csv')
+#dataset = dataset[, 2:3]
+
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                         levels = c('New York', 'California', 'Florida'),
+                         labels = c(1, 2, 3))
+
+# Splitting dataset into training set and test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# Feature Scaling
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# test_set[, 2:3] = scale(test_set[, 2:3])
